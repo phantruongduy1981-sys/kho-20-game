64586051a4a8e7dd0949a0c6befd8fc0461b3153
phantruongduy1981-sys <phantruongduy1981@gmail.com>
2025-12-16 11:53:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const games = [
     {
         name: "QUẢN LÝ THI ĐUA",
         icon: "fa-solid fa-briefcase",
-        url: "https://script.google.com/macros/s/AKfycbydYEY1582MSw7ygzT6090UWx6yBfcelxOrathg6tbbzof7JWqWET4FLCT2Bh3InAGbag/exec",
+        url: "https://script.google.com/macros/s/AKfycbyU6i_3CfCeFPJmsTYG9qXOOqXW5D9pc7aQNivXjKF0DyU8Vhz22cYN3ebxUwUXdphaJA/exec",
         color: "#a78bfa" // Màu tím
     },
 
@@ -93,6 +93,7 @@ if (grid) {
 } else {
     console.error("Lỗi: Không tìm thấy khung chứa game (id='game-grid')");
 }
+
 
 
 
