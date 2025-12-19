417c82160c3fa782aa54f6a89a9f48da054a267a
phantruongduy1981-sys <phantruongduy1981@gmail.com>
2025-12-19 15:04:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,9 +51,9 @@ const games = [
     },
          // --- GAME SỐ 7 ---
     {
-        name: "HỆ THỐNG LMS 11",
-        icon: "fa-solid fa-pen-to-square",
-        url: "LINK",
+        name: "GAME RỒNG VÀNG",
+        icon: "fa-solid fa-dragon",
+        url: "https://script.google.com/macros/s/AKfycbwsSX__CGYhSUoHNIZHwcMgho3eCjgUTgLYB8ZBpDcgCNP35v53tOghYE2s-l91bAvm/exec",
         color: "#f87171" // Màu đỏ
     }
 ];
@@ -93,6 +93,7 @@ if (grid) {
 } else {
     console.error("Lỗi: Không tìm thấy khung chứa game (id='game-grid')");
 }
+
 
 
 
